01692f80d0ef1b29ee3bae54478972ad783664ea
Lucian Lature <lucian.lature@gmail.com>
2017-02-16 20:32:32 +0000

    Saving Yarn cache into docker image.

--- Dockerfile
+++ Dockerfile
@@ -13,11 +13,22 @@ WORKDIR /usr/src/sitemanager-frontend
 ONBUILD ARG NODE_ENV
 ONBUILD ENV NODE_ENV $NODE_ENV
 
-COPY package.json /usr/src/sitemanager-frontend
-COPY yarn.lock /usr/src/sitemanager-frontend
+# COPY package.json /usr/src/sitemanager-frontend
+# COPY yarn.lock /usr/src/sitemanager-frontend
+
+ADD package.json yarn.lock /tmp/
+RUN mkdir -p /tmp/tools && mkdir -p /tmp/tools/scripts
+COPY ./tools/scripts /tmp/tools/scripts
+
+# Copy cache contents (if any) from local machine
+COPY .yarn-cache.tgz /usr/src/sitemanager-frontend
 
 COPY . /usr/src/sitemanager-frontend
-RUN yarn install
+
+# Install packages
+RUN cd /tmp && yarn
+RUN cd /usr/src/sitemanager-frontend && ln -s /tmp/node_modules
+
 
 RUN yarn run build
 EXPOSE 1337
--- tools/scripts/build_docker.sh
+++ tools/scripts/build_docker.sh
@@ -8,12 +8,12 @@ if [ ! -f .yarn-cache.tgz ]; then
   tar cvzf .yarn-cache.tgz --files-from /dev/null
 fi
 
-docker build . -t lucianlature/sitemanager
+docker build . -t lucianlature/dev_sitemanager
 
-docker run --rm --entrypoint cat lucianlature/sitemanager:latest /tmp/yarn.lock > /tmp/yarn.lock
+docker run --rm --entrypoint cat lucianlature/dev_sitemanager:latest /tmp/yarn.lock > /tmp/yarn.lock
 if ! diff -q yarn.lock /tmp/yarn.lock > /dev/null  2>&1; then
   echo "Saving Yarn cache"
-  docker run --rm --entrypoint tar lucianlature/sitemanager:latest czf - /root/.yarn-cache/ > .yarn-cache.tgz
+  docker run --rm --entrypoint tar lucianlature/dev_sitemanager:latest czf - /root/.yarn-cache/ > .yarn-cache.tgz
   echo "Saving yarn.lock"
   cp /tmp/yarn.lock yarn.lock
 fi
